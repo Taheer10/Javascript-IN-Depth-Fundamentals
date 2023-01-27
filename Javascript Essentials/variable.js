a1508c9226486df8993db8e73becc1e10eab4335
Tahir <Tahirhussain32145@gmail.com>
2023-01-27 08:40:54 +0545

    Variables in Js Essentials

---
+++ Javascript Essentials/variable.js
@@ -0,0 +1,19 @@
+// Variables are values in the code that can represent different values each
+// time the code runs.
+let name = "admin";
+name = "change";
+console.log(name);
+
+// let, var, and const
+// A variable definition consists of three parts: a variable-defining keyword (let, var,
+// or const), a name, and a value
+
+let nr1 = 12;
+var nr2 = 8;
+const PI = 3.14159;
+
+// var has global scope and let has block scope
+// var's global scope means that you can
+// use the variables defined with var in the entire script. On the other hand, let's block
+// scope means you can only use variables defined with let in the specific block of code
+// in which they were defined.
