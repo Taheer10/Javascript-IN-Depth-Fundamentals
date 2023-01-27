147a3d866665f946a9350029742534fb35063e9c
Tahir <Tahirhussain32145@gmail.com>
2023-01-27 08:27:46 +0545

    Getting started with js

---
+++ Getting Started With Javascript/external.js
@@ -0,0 +1,12 @@
+// JavaScript is an interpreted language, which means that the computer understands
+// it while running it
+
+console.log("Hey I am External");
+// prompt("Enter number");
+
+/*lorefdsffffff
+fsdsfdsf
+fsdsd */ //--Multiline comment
+
+console.log(Math.random()); //Generates  random number between 0 to 1
+console.log(Math.floor(Math.random() * 10)); //Math.floor removes the decimal value and prints integer values
