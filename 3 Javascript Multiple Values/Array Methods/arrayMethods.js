ecf1c148f1e7bc2d3f5492b4cb8ddb663bdcdf14
Tahir <Tahirhussain32145@gmail.com>
2023-01-31 18:22:57 +0545

    Adding elemets

--- 3 Javascript Multiple Values/Array Methods/arrayMethods.js
+++ 3 Javascript Multiple Values/Array Methods/arrayMethods.js
@@ -1,2 +1,13 @@
 //Array Methods
 
+//Adding and replacing elements
+
+let fruits = ["apple", "orange", "mango"];
+
+console.log(fruits.push("Banana"));
+// console.log(fruits);
+
+//Splice is used to insert elements in between certain index
+
+fruits.splice(1, 0, "Litchi");
+console.log(fruits);
