7a1ea6d7c7fa4f92ff9a37b49baed5523dc1900e
Tahir <Tahirhussain32145@gmail.com>
2023-02-17 17:13:29 +0545

    The dom -- html crash course

---
+++ 9 THE DOM/1 Html Crash Course/script.js
@@ -0,0 +1,4 @@
+//  It grabs an HTML page and turns it into a logical tree.
+
+// With JavaScript, we can select and manipulate parts of the DOM.
+//  This leads to interactive web pages instead of static ones.
---
+++ 9 THE DOM/2 The BOM/script.js
@@ -0,0 +1,10 @@
+// The BOM(Browser Object Model), sometimes also called the window browser object
+//It communicates with the Browser
+
+// The BOM holds all the methods and properties for JavaScript to interact with the browser.
+// This is information related  to previous pages visited, the size of the window of the browser, and also the DOM.
+
+//Some of the most important objects of the BOM
+// • History
+// • Navigator
+// • Location
